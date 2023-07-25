5734013e68838d35fb25136bace088b1a3da8caf
Youngjin Jo <yjinjo@berkeley.edu>
2023-07-25 13:20:04 +0900

    fix: remove comments

--- src/spaceone/inventory/service/collector_service.py
+++ src/spaceone/inventory/service/collector_service.py
@@ -82,28 +82,6 @@ class CollectorService(BaseService):
             )
             yield error_resource_response.to_primitive()
 
-        # Execute manager (Multithreading)
-        # with concurrent.futures.ThreadPoolExecutor(max_workers=MAX_WORKER) as executor:
-        #     future_executors = []
-        #     for execute_manager in self.execute_managers:
-        #         _manager = self.locator.get_manager(execute_manager)
-        #         future_executors.append(
-        #             executor.submit(_manager.collect_resources, params)
-        #         )
-        #
-        #     for future in concurrent.futures.as_completed(future_executors):
-        #         try:
-        #             for result in future.result():
-        #                 yield result.to_primitive()
-        #         except Exception as e:
-        #             _LOGGER.error(
-        #                 f"[collect] failed to yield result => {e}", exc_info=True
-        #             )
-        #             error_resource_response = self.generate_error_response(
-        #                 e, "", "inventory.Error"
-        #             )
-        #             yield error_resource_response.to_primitive()
-
         # Execute manager
         future_executors = []
         for execute_manager in self.execute_managers:
